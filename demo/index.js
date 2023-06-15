8f063ce032b93b8523bc6330120f4b8e701e3ac0
Carolina Wright <carolina.wright@mulesoft.com>
2023-06-15 16:47:44 -0300

    fix: remove internals usage

--- demo/index.js
+++ demo/index.js
@@ -12,8 +12,6 @@ import '../anypoint-checkbox.js';
 /** @typedef {import('../index').AnypointCheckbox} AnypointCheckbox */
 /** @typedef {import('@polymer/iron-form').IronFormElement} IronFormElement */
 
-const hasFormAssociatedElements = 'attachInternals' in document.createElement('span');
-
 class ComponentDemo extends ArcDemoPage {
   constructor() {
     super();
@@ -97,7 +95,8 @@ class ComponentDemo extends ArcDemoPage {
     const {
       demoStates,
       darkThemeActive,
-      demoDisabled
+      demoDisabled,
+      demoRequired
     } = this;
     return html`<section class="documentation-section">
       <h3>Interactive demo</h3>
@@ -273,10 +272,6 @@ class ComponentDemo extends ArcDemoPage {
 
         Learn more: <a href="https://www.chromestatus.com/feature/4708990554472448" target="_blank">Chrome status</a>
       </p>
-
-      ${hasFormAssociatedElements ?
-          html`<p>Your browser support this API</p>` :
-          html`<p>Your browser <b>does not</b> support this API</p>`}
 
       <div class="centered">
         <form id="nativeForm" @submit="${this._nativeSubmit}">
